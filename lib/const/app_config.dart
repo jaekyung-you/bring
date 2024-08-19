2457cbf61e4f43fb8b9fd9611d41126ccdee877d
jaekyungyou <youjk96@gmail.com>
2024-08-19 23:06:22 +0900

    appconfig, color theme 추가

---
+++ lib/const/app_config.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  static const borderRadiusMain = 16.0;
+  static const borderRadiusSub = 8.0;
+
+  static const innerPadding = 16.0;
+  static const contentPadding = 8.0;
+
+}
---
+++ lib/const/theme.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class BringColor {
+  static const primaryNavy = Color(0xff405D72);
+
+  static const primaryGrey = Color(0xff758694);
+
+  static const primaryBeige = Color(0xffF7E7DC);
+
+  static const primaryWhite = Color(0xfffff8f3);
+
+}
